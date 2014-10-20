b8339b6cb07ab672eff23fd06fb800816f0f19d9
Pete Argent <pete@thecoderfactory.com>
2014-10-20 18:17:00 +1100

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141015090818) do
+ActiveRecord::Schema.define(version: 20141015093105) do
 
   create_table "business_profiles", force: true do |t|
     t.string   "name"
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 20141015090818) do
 
   add_index "business_users", ["business_profile_id"], name: "index_business_users_on_business_profile_id"
   add_index "business_users", ["user_id"], name: "index_business_users_on_user_id"
+
+  create_table "user_profiles", force: true do |t|
+    t.string   "name"
+    t.string   "profile_pic"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
