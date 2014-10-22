e967561163bf78be5d5ed18c796317af5f154880
Timmo <timmo@Timmos-Macbook.local>
2014-10-22 20:39:51 +1100

    Added business profile

--- app/controllers/business_profiles_controller.rb
+++ app/controllers/business_profiles_controller.rb
@@ -25,7 +25,8 @@ class BusinessProfilesController < ApplicationController
   # POST /business_profiles.json
   def create
     @business_profile = BusinessProfile.new(business_profile_params)
-
+    @business_profile.users << current_user
+    
     respond_to do |format|
       if @business_profile.save
         format.html { redirect_to @business_profile, notice: 'Business profile was successfully created.' }
